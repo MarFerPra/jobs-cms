80898b82d47121ab9849870b1019ae07b6037dd7
marcofp <mfernandezpranno@gmail.com>
2016-12-17 18:33:22 +0100

    Created category model and associations with jobs. Closes #3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217163039) do
+ActiveRecord::Schema.define(version: 20161217165721) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +24,8 @@ ActiveRecord::Schema.define(version: 20161217163039) do
     t.boolean  "active"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_jobs_on_category_id"
   end
 
 end
